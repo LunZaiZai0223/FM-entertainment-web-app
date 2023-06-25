0419093b4f05002da2aaac1b3b6e62dd6865bde5
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-06-25 20:06:13 +0800

    refactor: Refactor styled-components settings

--- src/styled.d.ts
+++ src/styled.d.ts
@@ -1,17 +1,27 @@
+// 額外建立 styled.d.ts 建立 theme 所需要的屬性
+// 但是這樣子做就必須在這邊的 interface 建立屬性後 theme 中的屬性也要建立對應的屬性（所以一次就要改兩邊的檔案）
+// declare module 'styled-components' {
+//   export interface DefaultTheme {
+//     colors: {
+//       primary: string
+//       secondary: string
+//       white: string
+//       steelBlue: string
+//       tealBlue: string
+//       emeraldGreen: string
+//       fieryRed: string
+//     }
+//     fonts: string
+//   }
+// }
+
+// 改成用 typeof 讓 TypeScript 直接拿 theme 的屬性推斷型別（只要改 theme 的屬性即可）
+// ref. https://pjchender.dev/npm/npm-styled-components/#%E4%BD%BF%E7%94%A8-theme-%E5%92%8C-themeprovider
+import theme from './styles/theme'
 import 'styled-components'
 
-// 額外建立 styled.d.ts 建立 theme 所需要的屬性
 declare module 'styled-components' {
-  export interface DefaultTheme {
-    colors: {
-      primary: string
-      secondary: string
-      white: string
-      steelBlue: string
-      tealBlue: string
-      emeraldGreen: string
-      fieryRed: string
-    }
-    fonts: string
-  }
+  type Theme = typeof theme
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  export interface DefaultTheme extends Theme {}
 }
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,22 @@
-import { DefaultTheme } from 'styled-components'
+// import { DefaultTheme } from 'styled-components'
 
-const theme: DefaultTheme = {
+// const theme: DefaultTheme = {
+//   colors: {
+//     primary: '#11141E',
+//     secondary: '#171E31',
+//     white: '#FFFFFF',
+//     steelBlue: '#5A6A90',
+//     tealBlue: '#0D7490',
+//     emeraldGreen: '#11766E',
+//     fieryRed: '#FC4747',
+//   },
+//   fonts: 'Outfit, sans-serif',
+// }
+//
+// export default theme
+
+// 這樣子就不需要再拿擴展後 DefaultTheme interface
+const theme = {
   colors: {
     primary: '#11141E',
     secondary: '#171E31',
