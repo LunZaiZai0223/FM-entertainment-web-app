185df0b744b1d01ac9aaf15a63709337608a48fa
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-06-25 18:14:17 +0800

    feat: Use style-components settings

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,17 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { ThemeProvider } from 'styled-components'
+
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
+
+// components
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<App />)
+root.render(
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <App />
+  </ThemeProvider>,
+)
