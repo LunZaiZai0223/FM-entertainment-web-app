cf180889f79bba48f0b89b808aed86ec920f84fe
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-06-25 21:13:44 +0800

    chore: Create RouterPathMap constant

---
+++ src/constants/routerPathMap.constant.ts
@@ -0,0 +1,8 @@
+import { RouterPathNameEnum } from '../enums/routerPathName.enum'
+
+export const RouterPathMap = {
+  HOME: () => RouterPathNameEnum.HOME,
+  MOVIES: () => RouterPathNameEnum.MOVIES,
+  TVS: () => RouterPathNameEnum.TVS,
+  SEARCH: () => RouterPathNameEnum.SEARCH,
+}
