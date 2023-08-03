757be5b8861999cc3a8a3edb39b608ef0edf3be0
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-08-03 23:36:59 +0800

    feat: Add tmdb endpoints

--- src/constants/endpointPaths.ts
+++ src/constants/endpointPaths.ts
@@ -5,6 +5,23 @@ export const MovieEndpointPath = {
   Detail: (movieId: string) => {
     return `/movie/${movieId}`
   },
+  Popular: '/movie/popular',
+  Playing: '/movie/now_playing',
+  Upcoming: '/movie/upcoming',
+  TopRated: '/movie/top_rated',
+}
+
+export const TvEndpointPath = {
+  Trending: (isDay = true) => {
+    return `/trending/tv/${isDay ? 'day' : 'week'}`
+  },
+  Detail: (tvSeriesId: string) => {
+    return `/tv/${tvSeriesId}`
+  },
+  Popular: '/tv/popular',
+  AiringToday: '/tv/airing_today',
+  OnTheAir: '/tv/on_the_air',
+  TopRated: '/tv/top_rated',
 }
 
 export const TmdbImageEndpoint = 'https://image.tmdb.org/t/p/original'
