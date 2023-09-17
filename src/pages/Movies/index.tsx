3178980e1e4602e4461c3cfe6b9bb915eb0e6f52
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-09-17 21:18:17 +0800

    feat: Get genres from API

--- src/pages/Movies/index.tsx
+++ src/pages/Movies/index.tsx
@@ -1,8 +1,20 @@
+import { useQuery } from 'react-query'
+
+// apis
+import { getMovieGenresRequest } from '../../requests/tmdb'
+
 // components
 import CategoryList from '../../components/CategoryList'
+import Loader from '../../components/Loader'
 
 const Movies = () => {
-  return <CategoryList />
+  const { data, isLoading } = useQuery('movieGenreList', getMovieGenresRequest)
+
+  if (isLoading) {
+    return <Loader />
+  }
+
+  return <CategoryList genreList={data?.genres ?? []} />
 }
 
 export default Movies
