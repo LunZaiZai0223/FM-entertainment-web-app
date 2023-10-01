9cecd581f802f21474a5227d9fc716e46a59342f
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-10-02 00:26:06 +0800

    feat: Create constant
    - mediaTypes

---
+++ src/constants/mediaTypes.constants.ts
@@ -0,0 +1,16 @@
+const MOVIE = 'MOVIE'
+const TV_SERIES = 'TV_SERIES'
+const MULTI = 'MULTI'
+
+export const MEDIA_TYPE = {
+  MOVIE,
+  TV_SERIES,
+  MULTI,
+}
+
+export const MEDIA_TYPE_TITLE = {
+  MOVIE: 'Movie',
+  TV_SERIES: 'TV series',
+}
+
+export type MediaTypeTitle = 'MOVIE' | 'TV_SERIES'
