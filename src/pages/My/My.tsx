e7748aefaf978471bf8d352b79cf3c8e9d85a4cf
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-10-29 15:38:33 +0800

    feat: Implement page component <My />

---
+++ src/pages/My/My.tsx
@@ -0,0 +1,68 @@
+import { NavLink, Outlet, useLocation } from 'react-router-dom'
+
+// constants
+import { RouterPathMap } from '../../constants/routerPathMap.constant'
+
+// components
+import TabMenu from '../../components/UI/TabMenu'
+
+// styles
+import { Container } from './My.style'
+
+// local constants
+const FAVORITE = 1
+const BASIC_SETTINGS = 2
+
+const getCurrentTabIndex = (currentPathname: string) => {
+  switch (currentPathname) {
+    case RouterPathMap.MY_BASIC_SETTINGS():
+      return BASIC_SETTINGS
+
+    default:
+      return FAVORITE
+  }
+}
+
+const My = () => {
+  const { pathname } = useLocation()
+  const activatedTabIndex = getCurrentTabIndex(pathname)
+
+  const tabContentList = [
+    {
+      title: <NavLink to={''}>Favorite</NavLink>,
+      content: <Outlet />,
+      value: FAVORITE,
+    },
+    {
+      title: <NavLink to={'basic-settings'}>Basic Settings</NavLink>,
+      content: <Outlet />,
+      value: BASIC_SETTINGS,
+    },
+  ]
+
+  return (
+    <Container>
+      <TabMenu activatedTab={activatedTabIndex}>
+        <TabMenu.TabList
+          items={tabContentList}
+          renderItem={(item) => {
+            return (
+              <TabMenu.TabItem key={item.value} item={item}>
+                {item.title}
+              </TabMenu.TabItem>
+            )
+          }}
+        />
+        {tabContentList.map((item, index) => {
+          return (
+            <TabMenu.TabContent key={index} value={item.value}>
+              {item.content}
+            </TabMenu.TabContent>
+          )
+        })}
+      </TabMenu>
+    </Container>
+  )
+}
+
+export default My
