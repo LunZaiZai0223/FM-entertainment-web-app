333975e1b1eb649579f56456b66c1b11687cd685
LunZaiZai0223 <liu.wei.lun0223@gmail.com>
2023-12-02 18:11:20 +0800

    refactor: Move shared provider into root

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,27 @@
 import ReactDOM from 'react-dom/client'
 
+import { ThemeProvider } from 'styled-components'
 import { RouterProvider } from 'react-router-dom'
+import { QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
 import ToastProvider from './components/UI/Toast/ToastProvider'
+import Toast from './components/UI/Toast'
 
 import router from './router/'
+import { queryClient } from './utils/queryClient'
+import theme from './styles/theme'
+import GlobalStyles from './styles/global'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <ToastProvider>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Toast />
+        <RouterProvider router={router} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </ThemeProvider>
+    </QueryClientProvider>
   </ToastProvider>,
 )
--- src/layouts/BasicLayout/BasicLayout.tsx
+++ src/layouts/BasicLayout/BasicLayout.tsx
@@ -1,52 +1,30 @@
 import { Outlet } from 'react-router-dom'
 import { Suspense } from 'react'
 
-// libs
-import { ThemeProvider } from 'styled-components'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { ReactQueryDevtools } from 'react-query/devtools'
-
 // components
 import Navbar from '../../components/Navbar'
 import SearchBar from '../../components/SearchBar'
 import TopProgressBar from '../../components/TopProgressBar'
 import ScrollToTop from '../../components/UI/ScrollToTop'
-import Toast from '../../components/UI/Toast'
 
 // styles
-import theme from '../../styles/theme'
-import GlobalStyles from '../../styles/global'
 import { Main } from './BasicLayout.style'
 import { SkeletonTheme } from 'react-loading-skeleton'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false,
-      retry: false,
-    },
-  },
-})
-
 const BasicLayout = () => {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <ScrollToTop />
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Toast />
-        <SkeletonTheme baseColor='#202020' highlightColor='#444'>
-          <Suspense fallback={<TopProgressBar />}>
-            <Navbar />
-            <Main>
-              <SearchBar />
-              <Outlet />
-            </Main>
-          </Suspense>
-        </SkeletonTheme>
-      </ThemeProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+      <SkeletonTheme baseColor='#202020' highlightColor='#444'>
+        <Suspense fallback={<TopProgressBar />}>
+          <Navbar />
+          <Main>
+            <SearchBar />
+            <Outlet />
+          </Main>
+        </Suspense>
+      </SkeletonTheme>
+    </>
   )
 }
 
